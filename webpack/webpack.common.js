c123b059718b71f0aafdd869f52c194d8b195a25
Arnau García <arnau.garcia.gallego@gmail.com>
2018-01-19 13:04:59 +0100

    Created .example.files for properties and configured webpack.common.js for production settings

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -52,6 +52,17 @@ module.exports = (options) => ({
                 // If you use an API server, in `prod` mode, you will need to enable CORS
                 // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
                 SERVER_API_URL: `''`
+            },
+            'process.prod': {
+                NODE_ENV: `'${options.env}'`,
+                BUILD_TIMESTAMP: `'${new Date().getTime()}'`,
+                VERSION: `'${utils.parseVersion()}'`,
+                DEBUG_INFO_ENABLED: options.env === 'production',
+                // The root URL for API calls, ending with a '/' - for example: `"http://www.jhipster.tech:8081/myservice/"`.
+                // If this URL is left empty (""), then it will be relative to the current context.
+                // If you use an API server, in `prod` mode, you will need to enable CORS
+                // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
+                SERVER_API_URL: `'http://uplace.eu-west-3.elasticbeanstalk.com/'`
             }
         }),
         new webpack.optimize.CommonsChunkPlugin({
