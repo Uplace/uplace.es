4ca456ea5b18f80d2d85a9b889b96a9d93a4e475
Arnau García <arnau.garcia.gallego@gmail.com>
2018-01-23 08:47:12 +0100

    Add production URL when webpack:prod is triggered

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -51,7 +51,7 @@ module.exports = (options) => ({
                 // If this URL is left empty (""), then it will be relative to the current context.
                 // If you use an API server, in `prod` mode, you will need to enable CORS
                 // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
-                SERVER_API_URL: `''`
+                SERVER_API_URL: options.env === 'development' ? `''` : `'http://api.uplace.es/'`
             }
         }),
         new webpack.optimize.CommonsChunkPlugin({
