84ac8cb50bc654a4636f11646d7f926afa8d550e
Arnau García <arnau.garcia.gallego@gmail.com>
2018-01-23 12:57:18 +0100

    Implemented pagination on Notification #20

--- src/main/java/com/arnaugarcia/uplace/web/rest/NotificationResource.java
+++ src/main/java/com/arnaugarcia/uplace/web/rest/NotificationResource.java
@@ -6,9 +6,14 @@ import com.arnaugarcia.uplace.domain.Notification;
 import com.arnaugarcia.uplace.repository.NotificationRepository;
 import com.arnaugarcia.uplace.web.rest.errors.BadRequestAlertException;
 import com.arnaugarcia.uplace.web.rest.util.HeaderUtil;
+import com.arnaugarcia.uplace.web.rest.util.PaginationUtil;
 import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -80,14 +85,17 @@ public class NotificationResource {
     /**
      * GET  /notifications : get all the notifications.
      *
+     * @param pageable the pagination information
      * @return the ResponseEntity with status 200 (OK) and the list of notifications in body
      */
     @GetMapping("/notifications")
     @Timed
-    public List<Notification> getAllNotifications() {
-        log.debug("REST request to get all Notifications");
-        return notificationRepository.findAll();
-        }
+    public ResponseEntity<List<Notification>> getAllNotifications(Pageable pageable) {
+        log.debug("REST request to get a page of Notifications");
+        Page<Notification> page = notificationRepository.findAll(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/notifications");
+        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+    }
 
     /**
      * GET  /notifications/:id : get the "id" notification.
--- src/main/webapp/app/entities/notification/notification.route.ts
+++ src/main/webapp/app/entities/notification/notification.route.ts
@@ -1,4 +1,6 @@
-import { Routes } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
+import { JhiPaginationUtil } from 'ng-jhipster';
 
 import { UserRouteAccessService } from '../../shared';
 import { NotificationComponent } from './notification.component';
@@ -6,10 +8,29 @@ import { NotificationDetailComponent } from './notification-detail.component';
 import { NotificationPopupComponent } from './notification-dialog.component';
 import { NotificationDeletePopupComponent } from './notification-delete-dialog.component';
 
+@Injectable()
+export class NotificationResolvePagingParams implements Resolve<any> {
+
+    constructor(private paginationUtil: JhiPaginationUtil) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const page = route.queryParams['page'] ? route.queryParams['page'] : '1';
+        const sort = route.queryParams['sort'] ? route.queryParams['sort'] : 'id,asc';
+        return {
+            page: this.paginationUtil.parsePage(page),
+            predicate: this.paginationUtil.parsePredicate(sort),
+            ascending: this.paginationUtil.parseAscending(sort)
+      };
+    }
+}
+
 export const notificationRoute: Routes = [
     {
         path: 'notification',
         component: NotificationComponent,
+        resolve: {
+            'pagingParams': NotificationResolvePagingParams
+        },
         data: {
             authorities: ['ROLE_USER'],
             pageTitle: 'uplaceApp.notification.home.title'
