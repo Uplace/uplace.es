678090640014fe5fb6bc7d9b0367dc612745abff
Arnau García <arnau.garcia.gallego@gmail.com>
2018-01-26 08:46:19 +0100

    Implemented ServiceClass on Office #29 #28

--- src/main/java/com/arnaugarcia/uplace/service/OfficeService.java
+++ src/main/java/com/arnaugarcia/uplace/service/OfficeService.java
@@ -1,12 +1,28 @@
 package com.arnaugarcia.uplace.service;
 
 import com.arnaugarcia.uplace.domain.Office;
+import com.arnaugarcia.uplace.repository.OfficeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 /**
- * Service Interface for managing Office.
+ * Service Implementation for managing Office.
  */
-public interface OfficeService {
+@Service
+@Transactional
+public class OfficeService {
+
+    private final Logger log = LoggerFactory.getLogger(OfficeService.class);
+
+    private final OfficeRepository officeRepository;
+
+    public OfficeService(OfficeRepository officeRepository) {
+        this.officeRepository = officeRepository;
+    }
 
     /**
      * Save a office.
@@ -14,27 +30,41 @@ public interface OfficeService {
      * @param office the entity to save
      * @return the persisted entity
      */
-    Office save(Office office);
+    public Office save(Office office) {
+        log.debug("Request to save Office : {}", office);
+        return officeRepository.save(office);
+    }
 
     /**
      * Get all the offices.
      *
      * @return the list of entities
      */
-    List<Office> findAll();
+    @Transactional(readOnly = true)
+    public List<Office> findAll() {
+        log.debug("Request to get all Offices");
+        return officeRepository.findAll();
+    }
 
     /**
-     * Get the "id" office.
+     * Get one office by id.
      *
      * @param id the id of the entity
      * @return the entity
      */
-    Office findOne(Long id);
+    @Transactional(readOnly = true)
+    public Office findOne(Long id) {
+        log.debug("Request to get Office : {}", id);
+        return officeRepository.findOne(id);
+    }
 
     /**
-     * Delete the "id" office.
+     * Delete the office by id.
      *
      * @param id the id of the entity
      */
-    void delete(Long id);
+    public void delete(Long id) {
+        log.debug("Request to delete Office : {}", id);
+        officeRepository.delete(id);
+    }
 }
