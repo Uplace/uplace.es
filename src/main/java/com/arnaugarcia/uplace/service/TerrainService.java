271b4a02939253a4076a4f315b16be2d52b8bd34
Arnau García <arnau.garcia.gallego@gmail.com>
2018-01-26 08:22:32 +0100

    Implemented ServiceClass on Terrain #29 #28

--- src/main/java/com/arnaugarcia/uplace/service/TerrainService.java
+++ src/main/java/com/arnaugarcia/uplace/service/TerrainService.java
@@ -1,12 +1,28 @@
 package com.arnaugarcia.uplace.service;
 
 import com.arnaugarcia.uplace.domain.Terrain;
+import com.arnaugarcia.uplace.repository.TerrainRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 /**
- * Service Interface for managing Terrain.
+ * Service Implementation for managing Terrain.
  */
-public interface TerrainService {
+@Service
+@Transactional
+public class TerrainService {
+
+    private final Logger log = LoggerFactory.getLogger(TerrainService.class);
+
+    private final TerrainRepository terrainRepository;
+
+    public TerrainService(TerrainRepository terrainRepository) {
+        this.terrainRepository = terrainRepository;
+    }
 
     /**
      * Save a terrain.
@@ -14,27 +30,41 @@ public interface TerrainService {
      * @param terrain the entity to save
      * @return the persisted entity
      */
-    Terrain save(Terrain terrain);
+    public Terrain save(Terrain terrain) {
+        log.debug("Request to save Terrain : {}", terrain);
+        return terrainRepository.save(terrain);
+    }
 
     /**
      * Get all the terrains.
      *
      * @return the list of entities
      */
-    List<Terrain> findAll();
+    @Transactional(readOnly = true)
+    public List<Terrain> findAll() {
+        log.debug("Request to get all Terrains");
+        return terrainRepository.findAll();
+    }
 
     /**
-     * Get the "id" terrain.
+     * Get one terrain by id.
      *
      * @param id the id of the entity
      * @return the entity
      */
-    Terrain findOne(Long id);
+    @Transactional(readOnly = true)
+    public Terrain findOne(Long id) {
+        log.debug("Request to get Terrain : {}", id);
+        return terrainRepository.findOne(id);
+    }
 
     /**
-     * Delete the "id" terrain.
+     * Delete the terrain by id.
      *
      * @param id the id of the entity
      */
-    void delete(Long id);
+    public void delete(Long id) {
+        log.debug("Request to delete Terrain : {}", id);
+        terrainRepository.delete(id);
+    }
 }
