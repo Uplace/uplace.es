174617ddc6ef1a7ba9a91694b8dd4954f0f65cf1
Arnau García <arnau.garcia.gallego@gmail.com>
2018-02-01 11:24:42 +0100

    Implemented Criteria Inheritance on IndustrialPlant #40

--- src/main/java/com/arnaugarcia/uplace/service/dto/IndustrialPlantCriteria.java
+++ src/main/java/com/arnaugarcia/uplace/service/dto/IndustrialPlantCriteria.java
@@ -23,7 +23,7 @@ import io.github.jhipster.service.filter.StringFilter;
  * As Spring is unable to properly convert the types, unless specific {@link Filter} class are used, we need to use
  * fix type specific filters.
  */
-public class IndustrialPlantCriteria implements Serializable {
+public class IndustrialPlantCriteria extends PropertyCriteria implements Serializable {
     /**
      * Class for filtering EnergyCertificate
      */
