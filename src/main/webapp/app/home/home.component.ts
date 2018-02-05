017666ebc7988483888e234a16cd5f36b220bc87
Arnau García <arnau.garcia.gallego@gmail.com>
2018-02-05 14:40:36 +0100

    Black and white map and tested some new features #40

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,16 +1,17 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 import { Account, LoginModalService, Principal } from '../shared';
-import { AgmMap } from '@agm/core';
 import {HttpClient} from "@angular/common/http";
+import {AgmMap, MapTypeStyle} from '@agm/core';
 
 @Component({
     selector: 'up-home',
     templateUrl: './home.component.html',
     styleUrls: [
         'home.css'
-    ]
+    ],
+    encapsulation: ViewEncapsulation.None
 
 })
 
@@ -18,13 +19,16 @@ export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
     markers: any;
-
+    height: number;
+    @ViewChild('map') map: AgmMap;
+    public customStyle: MapTypeStyle[];
 
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
         private eventManager: JhiEventManager,
-        private http: HttpClient
+        private http: HttpClient,
+        private elementRef: ElementRef
     ) { }
 
     ngOnInit() {
@@ -36,6 +40,8 @@ export class HomeComponent implements OnInit {
             this.markers = result;
             console.log(result);
         });
+        this.customStyle = [ {"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"saturation":-100},{"visibility":"on"},{"lightness":10}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":50}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]}, {featureType:'road.highway',elementType:'all',stylers:[{hue:'#dddbd7'},{saturation:-92},{lightness:60},{visibility:'on'}]}, {featureType:'landscape.natural',elementType:'all',stylers:[{hue:'#c8c6c3'},{saturation:-71},{lightness:-18},{visibility:'on'}]},  {featureType:'poi',elementType:'all',stylers:[{hue:'#d9d5cd'},{saturation:-70},{lightness:20},{visibility:'on'}]} ];
+
     }
 
     registerAuthenticationSuccess() {
