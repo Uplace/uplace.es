8465f076b6c3ebd282ccef04c09995bf2165661f
Arnau García <arnau.garcia.gallego@gmail.com>
2018-03-06 12:30:37 +0100

    Fixed translations on admin-panel #105

--- src/main/webapp/app/layouts/admin-navbar/admin-navbar.component.ts
+++ src/main/webapp/app/layouts/admin-navbar/admin-navbar.component.ts
@@ -1,4 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
+import {JhiLanguageHelper, LoginModalService, LoginService, Principal} from "../../shared";
+import {VERSION} from "../../app.constants";
+import {JhiLanguageService} from "ng-jhipster";
+import {ProfileService} from "../profiles/profile.service";
+import {Router} from "@angular/router";
+import {NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     selector: 'up-admin-navbar',
@@ -6,15 +12,56 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AdminNavbarComponent implements OnInit {
 
+    inProduction: boolean;
+    languages: any[];
+    swaggerEnabled: boolean;
+    modalRef: NgbModalRef;
+    version: string;
 
     constructor(
-
+        private loginService: LoginService,
+        private languageService: JhiLanguageService,
+        private languageHelper: JhiLanguageHelper,
+        private principal: Principal,
+        private loginModalService: LoginModalService,
+        private profileService: ProfileService,
+        private router: Router,
+        private renderer: Renderer2
     ) {
-
+        this.version = VERSION ? 'v' + VERSION : '';
     }
 
     ngOnInit() {
+        this.languageHelper.getAll().then((languages) => {
+            this.languages = languages;
+        });
 
+        this.profileService.getProfileInfo().then((profileInfo) => {
+            this.inProduction = profileInfo.inProduction;
+            this.swaggerEnabled = profileInfo.swaggerEnabled;
+        });
+    }
+
+    changeLanguage(languageKey: string) {
+        this.languageService.changeLanguage(languageKey);
+    }
+
+    isAuthenticated() {
+        return this.principal.isAuthenticated();
+    }
+
+    login() {
+        this.modalRef = this.loginModalService.open();
+    }
+
+    logout() {
+        this.loginService.logout();
+        this.router.navigate(['']);
+    }
+
+
+    getImageUrl() {
+        return this.isAuthenticated() ? this.principal.getImageUrl() : null;
     }
 
 }
