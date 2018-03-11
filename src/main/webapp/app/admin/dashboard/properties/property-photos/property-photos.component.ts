87582b4df3693141def979d9a4b65633d92d6924
Arnau García <arnau.garcia.gallego@gmail.com>
2018-03-11 13:14:21 +0100

    Lited code and new structure for image upload #107

--- src/main/webapp/app/admin/dashboard/properties/property-photos/property-photos.component.ts
+++ src/main/webapp/app/admin/dashboard/properties/property-photos/property-photos.component.ts
@@ -13,83 +13,53 @@ import {FileUploader} from 'ng2-file-upload';
 
 export class PropertyPhotosComponent implements OnInit {
 
-    photo: Photo;
-
     @Input() photos: Photo[];
 
     @Output() photosChange: EventEmitter<Photo[]> = new EventEmitter<Photo[]>();
 
     public uploader: FileUploader = new FileUploader({url: ''});
-    public hasBaseDropZoneOver: boolean = false;
+    public hasBaseDropZoneOver = false;
+
+    constructor(
+
+    ) {
+        this.uploader.onAfterAddingFile = (item) => {
+            const reader = new FileReader();
+
+            var photo: Photo = new Photo();
+
+            photo.photoContentType = item._file.type;
+            photo.name = item._file.name;
+            photo.description = item._file.name;
+
+            reader.onload =  (event) => {
+
+                let imageData = reader.result;
+
+                let rawData = imageData.split('base64,');
+
+                if (rawData.length > 1) {
+
+                    photo.photo = rawData[1];
+
+                    this.photos.push(photo);
+                    this.photosChange.emit(this.photos);
+                    console.log(this.photos);
+                }
+            };
+            reader.readAsDataURL(item._file);
+        };
+    }
 
     public fileOverBase(e: any): void {
         this.hasBaseDropZoneOver = e;
-        if (!this.hasBaseDropZoneOver) {
-            let fileCount: number = this.uploader.queue.length;
-            if (fileCount > 0) {
-
-                this.uploader.queue.forEach((val, i, array) => {
-
-                    let fileReader = new FileReader();
-                    fileReader.onloadend = (e) => {
-
-                        let imageData = fileReader.result;
-                        let rawData = imageData.split("base64,");
-
-                        if (rawData.length > 1) {
-
-                            rawData = rawData[1];
-                            this.photos.push(new Photo(null, 'Test Photo', 'Test photo', 'image/jpeg', rawData, true, null));
-                            this.photosChange.emit(this.photos);
-                        }
-                    };
-                    fileReader.readAsDataURL(val._file);
-
-                });
-            }
-        }
-    }
-
-    constructor(
-        private dataUtils: JhiDataUtils,
-        private elementRef: ElementRef
-    ) {
-        this.photo = new Photo();
     }
 
     ngOnInit() {
         if (this.photos == null) {
             this.photos = <Photo[]>[];
         }
-    }
 
-    byteSize(field) {
-        return this.dataUtils.byteSize(field);
-    }
-
-    openFile(contentType, field) {
-        return this.dataUtils.openFile(contentType, field);
-    }
-
-    setFileData(event, entity, field, isImage) {
-        console.log(event);
-        console.log(entity);
-        console.log(field);
-        console.log(isImage);
-        this.dataUtils.setFileData(event, entity, field, isImage);
-        this.photo.name = 'Test photo';
-        this.photo.description = 'Test photo';
-        this.photo.thumbnail = true;
-        this.photos.push(this.photo);
-        this.photosChange.emit(this.photos);
-    }
-
-    clearInputImage(field: string, fieldContentType: string, idInput: string) {
-        this.dataUtils.clearInputImage(this.photo, this.elementRef, field, fieldContentType, idInput);
-    }
-
-    clear() {
-        console.log('Clear me');
     }
 
 }
