c774abe9870e8bdc6b4f80bd6790501bc7e40c3c
Arnau García <arnau.garcia.gallego@gmail.com>
2018-03-22 10:53:04 +0100

    Added property-price component on property-new #125

---
+++ src/main/webapp/app/admin/dashboard/properties/property-price/property-price.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'up-property-price',
+  templateUrl: './property-price.component.html',
+  styles: []
+})
+export class PropertyPriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
