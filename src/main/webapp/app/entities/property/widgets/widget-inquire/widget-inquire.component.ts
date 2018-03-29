e95bc0e7313a28c9cff800edce2877436f671808
Arnau García <arnau.garcia.gallego@gmail.com>
2018-03-29 13:06:53 +0200

    Added alert on creating request #127

--- src/main/webapp/app/entities/property/widgets/widget-inquire/widget-inquire.component.ts
+++ src/main/webapp/app/entities/property/widgets/widget-inquire/widget-inquire.component.ts
@@ -3,6 +3,7 @@ import {Property} from "../../property.model";
 import {Mail} from "../../../../shared/model/mail.model";
 import {PropertyService} from "../../property.service";
 import {Request, RequestOrigin} from "../../../../shared/request/request.model";
+import {JhiAlertService} from "ng-jhipster";
 
 @Component({
     selector: 'up-widget-inquire',
@@ -16,7 +17,8 @@ export class WidgetInquireComponent implements OnInit {
     request: Request = new Request();
 
     constructor(
-        private propertyService: PropertyService
+        private propertyService: PropertyService,
+        private alertService: JhiAlertService
     ) {
     }
 
@@ -27,7 +29,11 @@ export class WidgetInquireComponent implements OnInit {
         this.request.property = this.property;
         this.request.origin = RequestOrigin.WEB;
         this.propertyService.inquire(this.property.reference, this.request).subscribe((response) => {
-            console.log(response);
+            if (response.ok) {
+                this.alertService.success("Thanks for your interest");
+            } else {
+                this.alertService.error("error");
+            }
         });
     }
 
